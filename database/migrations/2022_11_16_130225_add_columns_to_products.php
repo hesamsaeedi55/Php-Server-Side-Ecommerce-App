177a5a06ef95c0c2c083adf83d1f3b1d5368bbfe
Hesam <118046088+hesamsaeedi55@users.noreply.github.com>
2022-12-16 01:35:46 +0330

    Add files via upload

---
+++ database/migrations/2022_11_16_130225_add_columns_to_products.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('brand_id')->after('id');
+            $table->string('image')->after('price');
+            $table->string('image2')->after('image');
+            $table->string('image3')->after('image2');
+            $table->boolean('available')->default(1)->after('image3');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2022_12_12_174834_create_wishlist_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wishlist', function (Blueprint $table) {
+            $table->id();
+            $table->integer('userID');
+            $table->integer('productID');
+            $table->string('name');
+            $table->string('image');
+            $table->decimal('price');
+            $table->integer('entity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wishlist');
+    }
+};
